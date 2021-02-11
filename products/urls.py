a8479a18834cc53a151f186712e160a1b88e5815
mattb0101 <mattbaum666@googlemail.com>
2021-02-11 18:53:54 +0000

    Added some images, started on the product info page and title on the main title page

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 # this imports views/py from current directory
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
+    path('', views.all_products, name='products'),
+    path('<item_id>', views.product_info, name='product_info')
 ]
