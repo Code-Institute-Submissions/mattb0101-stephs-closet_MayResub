4f75e2b07ea2fb77ea0c836a368ad93d746c4b4a
mattb0101 <mattbaum666@googlemail.com>
2021-02-13 16:14:32 +0000

    filtering categories set up and basics of sorting started

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -11,8 +11,29 @@ def all_products(request):
 
     products = Product.objects.all()
     search = None
+    categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__cat_name__in=categories)
+            categories = Category.objects.filter(cat_name__in=categories)
+
         if 'search' in request.GET:
             search = request.GET['search']
             if not search:
@@ -22,9 +43,13 @@ def all_products(request):
             searches = Q(name__icontains=search)
             products = products.filter(searches)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': search,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
