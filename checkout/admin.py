9196ddeb20d87d5c090a8c8ed0587d6f86954fff
mattb0101 <mattbaum666@googlemail.com>
2021-02-15 16:43:41 +0000

    Customised standard checkout form

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineItemAdminInLine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_subtotal',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInLine,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'grand_total',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -57,10 +57,10 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(
-        'Order', null=False, blank=False,
+        Order, null=False, blank=False,
         on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(
-        'Product', null=False, blank=False, on_delete=models.CASCADE)
+        Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_subtotal = models.DecimalField(
         max_digits=6, decimal_places=2, null=False,
