abeb0ece9c45b7571529286dde74a39d43935c3b
mattb0101 <mattbaum666@googlemail.com>
2021-02-16 12:31:13 +0000

    Stripe complete payment added

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,8 +1,8 @@
 
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1)
-var client_secret = $('#client_secret').text().slice(1, -1)
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1)
+var clientSecret = $('#client_secret').text().slice(1, -1)
 
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 
 var elements = stripe.elements();
 var style = {
@@ -21,6 +21,7 @@ var style = {
   }
 };
 
+// Realtime validations
 var card = elements.create("card", { style: style });
 card.mount("#card-element");
 
@@ -37,4 +38,34 @@ card.addEventListener('change', function(event) {
   } else {
     displayError.textContent = '';
   }
+});
+
+// Form Submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true });
+    $('#pay-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function (result) {
+        if (result.error) {
+            var displayError = document.getElementById('card-errors');
+            var html = `
+                <span role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(displayError).html(html);
+            card.update({ 'disabled': false });
+            $('#pay-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in the cart to checkout")
@@ -17,13 +20,21 @@ def checkout(request):
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, "Stripe public key is missing, please check")
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51IGOjBGOUjQjPXgdd2fKB37QuCgvdtHCf2i2FHvHLgKVwNj9yWqyrwmV98ZRtodfTMcNlaATNQWuSx7UsdpmchVS00EUVyqTkq',
-        'client_secret': 'test_client_secret'
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret
     }
 
     return render(request, template, context)
