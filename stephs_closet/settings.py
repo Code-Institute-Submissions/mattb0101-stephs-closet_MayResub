6f3028207fad17828b0f57ee80a91498f755ea45
mattb0101 <mattbaum666@googlemail.com>
2021-02-17 17:00:53 +0000

    Added new secret key

--- stephs_closet/settings.py
+++ stephs_closet/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#v6+n%n0s%39w%s4jsb%#f!f!d7up%l79vp1nqxlh$4upx74i@'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['stephs-closet.herokuapp.com', 'localhost']
 
