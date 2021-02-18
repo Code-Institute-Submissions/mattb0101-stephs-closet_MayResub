df0148a90a9916fa28c879c655827ae48335e7e6
mattb0101 <mattbaum666@googlemail.com>
2021-02-18 11:36:43 +0000

    Added header container in to deal with home page needing to push down

--- products/views.py
+++ products/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
@@ -16,9 +17,8 @@ def all_products(request):
     sub_categories = None
     sort = None
     direction = None
-
+    
     if request.GET:
-        print(request.GET)
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
@@ -58,12 +58,18 @@ def all_products(request):
 
     current_sorting = f'{sort}_{direction}'
 
+    paginator = Paginator(products, 4)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
         'products': products,
         'search_term': search,
         'current_categories': categories,
         'current_sub_categories': sub_categories,
         'current_sorting': current_sorting,
+        'page_obj': page_obj,
     }
 
     return render(request, 'products/products.html', context)
