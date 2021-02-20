b71ff14148351ef3dc43d104f50a75de2f2fe2d5
mattb0101 <mattbaum666@googlemail.com>
2021-02-20 23:30:29 +0000

    Added toasts to all forms of adding products and updating cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -13,7 +13,7 @@ def view_cart(request):
 def add_to_cart(request, item_id):
     """ add item and quantity to the cart"""
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -47,6 +47,7 @@ def add_to_cart(request, item_id):
 def update_cart(request, item_id):
     """ Update items already in the cart """
 
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'size' in request.POST:
@@ -56,15 +57,19 @@ def update_cart(request, item_id):
     if size:
         if quantity > 0:
             cart[item_id]['item_with_size'][size] = quantity
+            messages.success(request, f'Updated {product.name} quantity in your bag')
         else:
             del cart[item_id]['item_with_size'][size]
             if not cart[item_id]['item_with_size']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed {product.name} quantity in your bag')
     else:
         if quantity > 0:
             cart[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity in your bag')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} to your bag')
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
@@ -74,6 +79,7 @@ def remove_from_cart(request, item_id):
     """ Update items already in the cart """
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'size' in request.POST:
             size = request.POST['size']
@@ -83,12 +89,14 @@ def remove_from_cart(request, item_id):
             del cart[item_id]['item_with_size'][size]
             if not cart[item_id]['item_with_size']:
                 cart.pop(item_id)
+                messages.success(request, f'Removed {product.name} quantity in your bag')
         else:
             cart.pop(item_id)
+            messages.success(request, f'Removed {product.name} quantity in your bag')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.success(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
-        
