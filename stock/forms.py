74f9dbe91a330aff605d7054d74576207ed4fe1a
mattb0101 <mattbaum666@googlemail.com>
2021-02-20 17:30:39 +0000

    Added filter for stock management

--- stock/forms.py
+++ stock/forms.py
@@ -1,3 +1,8 @@
 from django import forms
 from .models import Stock
 
+
+class SearchForm(forms.ModelForm):
+    class Meta:
+        model = Stock
+        fields = ['product']
--- stock/views.py
+++ stock/views.py
@@ -1,26 +1,38 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Stock
+from .forms import SearchForm
+
+from products.models import Product
 
 
 def stock(request):
     """ View to show list of items and stock """
 
     stock = Stock.objects.all()
+    form = SearchForm(request.POST or None)
 
     context = {
         'stock': stock,
+        'form': form,
     }
+    if request.method == "POST":
+        stock = Stock.objects.filter(
+            product__icontains=form['product'].value())
 
+        context = {
+            'form': form,
+            'stock': stock,
+        }
     return render(request, 'stock/stock.html', context)
 
 
 def update_stock(request, item_id):
     """ Update stock of an item manually """
 
-    stock = get_object_or_404(Stock, pk=item_id)
+    item = get_object_or_404(Product, pk=item_id)
 
     context = {
-        'stock': stock,
+        'item': item,
     }
 
     return render(request, 'stock/update-stock.html', context)
