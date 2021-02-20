66fb0feb3ed0e217c1eb1ef7718f3b99a561ac9e
mattb0101 <mattbaum666@googlemail.com>
2021-02-20 15:54:54 +0000

    View Table of stock items in stock created

--- stock/models.py
+++ stock/models.py
@@ -6,3 +6,6 @@ from products.models import Product
 class Stock(models.Model):
     product = models.ForeignKey(Product, max_length=254, blank=False, null=False, on_delete=models.CASCADE)
     in_stock = models.IntegerField(blank=True, null=True, default=0)
+
+    def __str__(self):
+        return self.product
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Stock
 
 
 def stock(request):
-    """ View to create index page return """
-    return render(request, 'stock/stock.html')
+    """ View to show list of items and stock """
+
+    stock = Stock.objects.all()
+
+    context = {
+        'stock': stock,
+    }
+
+    return render(request, 'stock/stock.html', context)
+
+
+def update_stock(request, item_id):
+    """ Update the stock for an individual item manually """
+
+    stock = Stock.objects.all()
+
+    context = {
+        'stock': stock,
+    }
+
+    return redirect('stock/stock.html')
