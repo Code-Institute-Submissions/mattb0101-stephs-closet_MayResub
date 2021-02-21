105662d5d6476b1a4a6d2837b428a5fe7cdfb556
mattb0101 <mattbaum666@googlemail.com>
2021-02-21 14:52:39 +0000

    Delivery info update on profile page toast success changed

--- profile/views.py
+++ profile/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def user_profile(request):
     """ display users profile """
-    context = {}
+    account = get_object_or_404(UserAccount, user=request.user)
+
+    if request.method == "POST":
+        form = UserAccountForm(request.POST, instance=account)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserAccountForm(instance=account)
+    orders = account.orders.all
+
+    context = {
+        'form': form,
+        'orders': orders,
+        'on_account': True
+    }
 
     return render(request, 'profile/profile.html', context)
