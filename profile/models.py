989051d5800ab3a54a70ef2cdc96f69d2e553ee9
mattb0101 <mattbaum666@googlemail.com>
2021-02-21 12:40:58 +0000

    Created models for user account and updated checkout model

--- profile/models.py
+++ profile/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from django_countries.fields import CountryField
 
 
 class UserAccount(models.Model):
@@ -7,8 +11,22 @@ class UserAccount(models.Model):
     User Account that the customer will be
     able to see their order history
     """
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=False)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', max_length=80, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_profile(sender, instance, created, **kwargs):
+    """ Create or update profile """
+    if created:
+        UserAccount.objects.create(user=instance)
+    instance.userprofile.save()
