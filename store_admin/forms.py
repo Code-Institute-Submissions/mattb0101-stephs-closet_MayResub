21bc0151b5a50117de89a419cf4bfb84104cb355
mattb0101 <mattbaum666@googlemail.com>
2021-02-22 17:07:23 +0000

    Started admin product edit forms

---
+++ store_admin/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from products.models import Product, Category, Sub_Category
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __inti__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        cat_friendlynames = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = cat_friendlynames
+        for field_name, field in self.fields.items():
