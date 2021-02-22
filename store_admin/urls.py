2cfe5654dad68efb68ec107454144df7835c1425
mattb0101 <mattbaum666@googlemail.com>
2021-02-22 18:02:21 +0000

    Added add product form to store admin

--- store_admin/urls.py
+++ store_admin/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store_admin, name='store_admin')
+    path('', views.store_admin, name='store_admin'),
+    path('add/', views.add_product, name='add_product')
 ]
--- store_admin/views.py
+++ store_admin/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+from .forms import ProductForm
+
 
 def store_admin(request):
     """ View to create index page return """
     return render(request, 'store_admin/store_admin.html')
+
+
+def add_product(request):
+    """ Add a new product """
+    form = ProductForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'store_admin/add_product.html', context)
