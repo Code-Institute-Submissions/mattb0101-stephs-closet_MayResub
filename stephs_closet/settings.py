631708188c98f46938074255d45dde81941e9f6d
mattb0101 <mattbaum666@googlemail.com>
2021-02-24 13:57:15 +0000

    commented out facebook social providers as not functional and creating errors

--- stephs_closet/settings.py
+++ stephs_closet/settings.py
@@ -105,33 +105,33 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SOCIALACCOUNT_PROVIDERS = \
-    {'facebook':
-       {'METHOD': 'oauth2',
-        'SCOPE': ['email','public_profile', 'user_friends'],
-        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
-        'FIELDS': [
-            'id',
-            'email',
-            'name',
-            'first_name',
-            'last_name',
-            'verified',
-            'locale',
-            'timezone',
-            'link',
-            'gender',
-            'updated_time'],
-        'EXCHANGE_TOKEN': True,
-        'LOCALE_FUNC': lambda request: 'kr_KR',
-        'VERIFIED_EMAIL': False,
-        'VERSION': 'v2.4'}}
+# SOCIALACCOUNT_PROVIDERS = \
+#     {'facebook':
+#        {'METHOD': 'oauth2',
+#         'SCOPE': ['email','public_profile', 'user_friends'],
+#         'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+#         'FIELDS': [
+#             'id',
+#             'email',
+#             'name',
+#             'first_name',
+#             'last_name',
+#             'verified',
+#             'locale',
+#             'timezone',
+#             'link',
+#             'gender',
+#             'updated_time'],
+#         'EXCHANGE_TOKEN': True,
+#         'LOCALE_FUNC': lambda request: 'kr_KR',
+#         'VERIFIED_EMAIL': False,
+#         'VERSION': 'v2.4'}}
 
 SITE_ID = 1
 
-# Facebook
-SOCIAL_AUTH_FACEBOOK_KEY = '269685144526818'
-SOCAIL_AUTH_FACEBOOK_SECRET = '6734c9ef11d9b350d6cfe7e28b701cb7'
+# # Facebook
+# SOCIAL_AUTH_FACEBOOK_KEY = '269685144526818'
+# SOCAIL_AUTH_FACEBOOK_SECRET = '6734c9ef11d9b350d6cfe7e28b701cb7'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
