ae27b38288338c61268322690666dc2cbad7a600
mattb0101 <mattbaum666@googlemail.com>
2021-02-24 12:22:32 +0000

    styled up order confirmation form for use with standard orders and historical orders from user profile

--- stephs_closet/settings.py
+++ stephs_closet/settings.py
@@ -238,7 +238,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBAckend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'stephscloset@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
